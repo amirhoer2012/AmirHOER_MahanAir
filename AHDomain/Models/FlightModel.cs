f64c210c526629eca9a17a28f7c2eb205ae4d080
AH.Erambue <AH.Erambue@KS89>
2025-03-04 13:58:05 +0330

    Add project files.

---
+++ AHDomain/Models/FlightModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHDomain.Models
+{
+    public class FlightModel
+    {
+        public long Id { get; set; }
+        public string PlaneName { get; set; }
+        public AirPortModel Source { get; set; }
+        public AirPortModel Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int AvailableSeats { get; set; }
+    }
+}
---
+++ AHDomain/Models/ReservationModel.cs
@@ -0,0 +1,55 @@
+﻿namespace AHDomain.Models
+{
+    public enum ReservationState
+    {
+        NotReserved,
+        TempReserved,
+        Reserved,
+        Cancelled
+    }
+
+    public class ReservationModel
+    {
+        #region Ctor
+        
+        public ReservationModel(UserModel user, FlightModel flight)
+        {
+            this.User = user;
+            this.Flight = flight;
+            this.State = ReservationState.NotReserved;
+        } 
+
+        #endregion
+
+        public void ReserveFlight(int id, DateTime reserveDate)
+        {
+            this.Id = id;
+            this.ReserveDate = reserveDate;
+            this.State = ReservationState.TempReserved;
+        }
+
+        public void ConfirmReserve(DateTime confirmationDate)
+        {
+            this.ConfirmationDate = confirmationDate;
+            this.State = ReservationState.Reserved;
+        }
+
+        public void CancelReserve(DateTime cancellationDate)
+        {
+            this.CancellationDate = cancellationDate;
+            this.State = ReservationState.Cancelled;
+        }
+
+        #region Properties
+
+        public int Id { get; private set; }
+        public UserModel User { get; }
+        public FlightModel Flight { get; }
+        public ReservationState State { get; private set; }
+        public DateTime ReserveDate { get; private set; }
+        public DateTime? ConfirmationDate { get; private set; }
+        public DateTime? CancellationDate { get; private set; }
+
+        #endregion
+    }
+}
